14961f217a9308cc25293f4a390de257d822e493
muhammadrenauldi <renauldizanagi@gmail.com>
2023-05-29 22:48:14 +0700

    add new endpoint for moota

--- app/Http/Controllers/MootaClientController.php
+++ app/Http/Controllers/MootaClientController.php
@@ -160,4 +160,96 @@ class MootaClientController extends Controller
 
         return response()->json($data);
     }
+
+    public function getmutation(Request $request) {
+        $type = $request->input('type');
+        $bank = $request->input('bank');
+        $amount = $request->input('amount');
+        $description = $request->input('description');
+        $note = $request->input('note');
+        $date = $request->input('date');
+        $start_date = $request->input('start_date');
+        $end_date = $request->input('end_date');
+        $tag = $request->input('tag');
+        $page = $request->input('page');
+        $per_page = $request->input('per_page');
+        
+        $data = [
+            "current_page" => 1, 
+            "data" => [
+                  [
+                     "account_number" => "7062015211", 
+                     "date" => "2022-07-11 00:00:00", 
+                     "description" => "IBFT KREDIT BCA-SYA DI BCA", 
+                     "amount" => "100000.00", 
+                     "type" => "CR", 
+                     "note" => "", 
+                     "balance" => "0.00", 
+                     "created_at" => "2022-07-11 13:39:14", 
+                     "updated_at" => "2022-07-11 13:39:14", 
+                     "mutation_id" => "wG4kYJ6zpRD", 
+                     "token" => "wG4kYJ6zpRD", 
+                     "bank_id" => "4D0LWdYkevQ", 
+                     "taggings" => [
+                     ], 
+                     "bank" => [
+                           "corporate_id" => "YBMN", 
+                           "username" => "MAKER", 
+                           "atas_nama" => "Moota", 
+                           "balance" => "0.00", 
+                           "account_number" => "7062015211", 
+                           "bank_type" => "bsi", 
+                           "pkg" => "", 
+                           "login_retry" => 0, 
+                           "date_from" => "2022-07-11 00:00:00", 
+                           "date_to" => "2022-07-11 00:00:00", 
+                           "meta" => [
+                              "session_id" => "86c418da-d7b3-4d2b-b6aa-55cba45725a4", 
+                              "activity_summary" => "Ditemukan 36 mutasi dalam 108.59 detik" 
+                           ], 
+                           "interval_refresh" => 15, 
+                           "next_queue" => "2022-07-11 13:55:03", 
+                           "is_active" => true, 
+                           "in_queue" => 0, 
+                           "in_progress" => 0, 
+                           "is_crawling" => 1, 
+                           "recurred_at" => "2022-07-12 13:37:36", 
+                           "created_at" => "2022-07-11 13:37:36", 
+                           "token" => "4D0LWdYkevQ", 
+                           "bank_id" => "4D0LWdYkevQ", 
+                           "label" => "BSI", 
+                           "last_update" => "2022-07-11T06:40:03.000000Z", 
+                           "icon" => "http://moota.test/images/icon-bank-bsi.png" 
+                        ] 
+                  ] 
+               ], 
+            "first_page_url" => "http://moota.test/api/v2/mutation?page=1", 
+            "from" => 1, 
+            "last_page" => 617, 
+            "last_page_url" => "http://moota.test/api/v2/mutation?page=617", 
+            "links" => [
+                                 [
+                                    "url" => null, 
+                                    "label" => "Sebelumnya", 
+                                    "active" => false 
+                                 ], 
+                                 [
+                                       "url" => "http://moota.test/api/v2/mutation?page=1", 
+                                       "label" => "1", 
+                                       "active" => true 
+                                    ] 
+                              ], 
+            "next_page_url" => "http://moota.test/api/v2/mutation?page=2", 
+            "path" => "http://moota.test/api/v2/mutation", 
+            "per_page" => "1", 
+            "prev_page_url" => null, 
+            "to" => 1, 
+            "total" => 617,
+            "testparam" => [
+                $type, $bank, $amount, $description, $note, $date, $start_date, $end_date, $tag, $page, $per_page
+            ]
+        ]; 
+
+        return response()->json($data);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Accunt Nuber Sync
 Route::group(['prefix' => 'v2/bank/'], function () {
     route::get('/available', [MootaClientController::class, 'getbanklist']);
     Route::post('/store', [MootaClientController::class, 'index']);
     Route::post('/update/{bank_id}', [MootaClientController::class, 'updatebank']);
     Route::post('/{bank_id}/destroy', [MootaClientController::class, 'deletebank']);
+
+    // https://app.moota.co/api/v2/bank/update/bank_id
+    // https://app.moota.co/api/v2/bank&?page=1&per_page=10
+});
+
+// Mutation
+Route::group(['prefix' => 'v2/mutation/'], function () {
+    Route::get('/', [MootaClientController::class, 'getmutation']);
 
     // https://app.moota.co/api/v2/bank/update/bank_id
     // https://app.moota.co/api/v2/bank&?page=1&per_page=10
